7a2ec9135e888af19e13215a1a72e3ee8543ce02
kezia2909 <c14190048@john.petra.ac.id>
2023-03-24 02:08:32 +0700

    v3.1 result page oke

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "13f02a45f717136aaa4c50365de6118b",
-"/": "13f02a45f717136aaa4c50365de6118b",
-"main.dart.js": "ef86d70c99efe77ea7c293b181a0bf72",
+"index.html": "84d976d141c73a01fa9d7a057166c6a9",
+"/": "84d976d141c73a01fa9d7a057166c6a9",
+"main.dart.js": "2a2249aeb45b5368a35f8673c80a82a1",
 "manifest.json": "a3e9729f53b960debd39e19d5de2edd5",
 "version.json": "98a46061a37a1401e5ac04244fed4be8"
 };
